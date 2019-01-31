f0c343babc59755c90f927a3192a230d5fd049a7
David Ahn <dja1990@gmail.com>
2019-01-31 11:36:33 -0800

    drills02-completed countOccurrence and wordLengths functions

--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -1,14 +1,36 @@
 
 
-function countOccurences(){
-
+function countOccurences(arrInput, inputStr){
+    let counterObj = {};
+    for(let i = 0; i < arrInput.length; i++){
+        let eachWord = arrInput[i];
+        if(counterObj[eachWord] == undefined){
+            counterObj[eachWord] = 1;
+        } else {
+            counterObj[eachWord] += 1;
+        }
+        // counterObj[eachWord] = counterObj[eachWord] ? counterObj[eachWord] + 1 : 1;
+        // ternary way ^
+        }
+    let result = counterObj[inputStr];
+    if(result === undefined){
+        return 0;
+    }
+    return result;
 }
 
-function wordLengths(){
-
+function wordLengths(wordArr){
+    let resultArr = [];
+    for(let i = 0; i < wordArr.length; i++){
+        let eachWordLength = wordArr[i].length;
+        resultArr.push(eachWordLength);
+    }
+    return resultArr;
 }
 
-function getMinMaxMean(){
+function getMinMaxMean(numArr){
+    let resultsObj = {};
+    let minVal = Math.min(numArr)
 
 }
 

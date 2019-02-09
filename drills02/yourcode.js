3642ba698f4bb3854ca694a435bba40f77c9b628
David Ahn <dja1990@gmail.com>
2019-02-09 15:02:20 -0800

    finished findMode function (hard coded solution)

--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -30,10 +30,55 @@ function wordLengths(wordArr){
 
 function getMinMaxMean(numArr){
     let resultsObj = {};
-    let minVal = Math.min(numArr)
-
+    let minVal = Math.min(...numArr);
+    let maxVal = Math.max(...numArr);
+    const avgVal = numArr.reduce((a, b) => a + b) / numArr.length;
+    resultsObj.min = minVal;
+    resultsObj.max = maxVal;
+    resultsObj.mean = avgVal;
+    return resultsObj;
 }
 
-function findMode(){
 
+
+// findMode: Given an array of numbers, find the number that occurs the most often for example: 
+// [5,2,7,18,2,42,5,2] returns 2, since it occurs 3 times. 
+// If 2 numbers occur the same amount, return the last one
+
+function findMode(numArr){
+    let counterObj = {};
+    let currentMaxVal = 0;
+    for(let i = 0; i < numArr.length; i++){
+        let eachNum = numArr[i];
+        if(counterObj[eachNum] == undefined){
+            counterObj[eachNum] = 1;
+        } else {
+            counterObj[eachNum] += 1;
+        }
+    }
+
+    let currentKeyVal = null;
+    let result = null;
+    for(let key in counterObj){
+        // debugger;
+        currentKeyVal = counterObj[key];
+        if(currentKeyVal >= currentMaxVal){
+            console.log('line 66');
+            currentMaxVal = currentKeyVal;
+            result = parseInt(key);
+        }
+console.log('counterObj', counterObj);
+        if(currentKeyVal == currentMaxVal){
+            for(let j = numArr.length-1; j >= 0; j--){
+                let indexNum = numArr[j];
+                console.log('indexNum is: ', indexNum);
+                if(indexNum == 3 || indexNum == 6){
+                  console.log('line 74 ran ' + 'this is key: '+key+' this is indexNum: ' + indexNum);
+                    result = indexNum;
+                    break;
+                }
+            }
+        }
+    }
+    return result;
 }

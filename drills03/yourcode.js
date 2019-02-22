af08200deb2c4e5c28c56c5b3d2fd02e05422860
David Ahn <dja1990@gmail.com>
2019-02-21 16:34:18 -0800

    getPath function complete

--- drills03/yourcode.js
+++ drills03/yourcode.js
@@ -1,10 +1,29 @@
 
 
-function getPath(){
-
+function getPath(strPath){
+    let arrPath = strPath.split('');
+    let i = 0;
+    let output = [];
+    let results = "";
+    while(i <= arrPath.length){
+        let eachChar = arrPath[i];
+        if(eachChar !== "/") {
+            if(eachChar === undefined) {
+                output.push(results);
+                break;
+            }
+            results += eachChar;
+        }
+        if(eachChar === "/") {
+            output.push(results);
+            results = "";
+        }
+        i++;
+    }
+    return output;
 }
 
-function getPathParts(){
+function getPathParts(pathStr){
 
 }
 
